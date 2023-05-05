906e7a8b28608a16cc0ae65ad69dd6d4cb7e400d
Jakub Surdej <jakub.surdej@codahead.com>
2023-05-05 14:37:49 +0200

    chore: initialize project

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  roots: ['<rootDir>'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
