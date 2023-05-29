34c0b89d73b4fd2c2d39675d59fe271a2e299578
Jakub Surdej <jakub.surdej@codahead.com>
2023-05-29 13:52:37 +0200

    feat: add function to update expiration of domain in datbase

--- src/domains/domains.service.spec.ts
+++ src/domains/domains.service.spec.ts
@@ -1,22 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbService } from '@src/db/db.service';
 import { DomainsService } from './domains.service';
-
-const testDomain = {
-  id: '1',
-  name: 'example.com',
-  userId: 'test-user-id',
-};
-
-const mockDbService = {
-  domain: {
-    create: jest.fn().mockResolvedValue(testDomain),
-    findMany: jest.fn().mockResolvedValue([testDomain]),
-    findFirst: jest.fn().mockResolvedValue(testDomain),
-    findUnique: jest.fn().mockResolvedValue(testDomain),
-    delete: jest.fn().mockResolvedValue(testDomain),
-  },
-};
+import { DomainsExpirationService } from './domains-expiration/domains-expiration.service';
+import { NotFoundException, HttpException } from '@nestjs/common';
 
 describe(DomainsService.name, () => {
   let service: DomainsService;
@@ -28,6 +14,10 @@ describe(DomainsService.name, () => {
         {
           provide: DbService,
           useValue: mockDbService,
+        },
+        {
+          provide: DomainsExpirationService,
+          useValue: mockExpirationService,
         },
       ],
     }).compile();
@@ -66,4 +56,60 @@ describe(DomainsService.name, () => {
       expect(result).toEqual(testDomain);
     });
   });
+
+  describe('updateExpirationDate method', () => {
+    beforeEach(() => {
+      mockDbService.domain.update = jest.fn().mockResolvedValue(testExpirationDateDomain);
+    });
+
+    it('should update the expiration date of a domain for a user', async () => {
+      const result = await service.updateExpirationDate(testDomain.id, testDomain.userId);
+      expect(result).toEqual(testExpirationDateDomain);
+      expect(mockDbService.domain.findUnique).toHaveBeenCalledWith({ where: { id: testDomain.id } });
+      expect(mockExpirationService.getExpirationDate).toHaveBeenCalledWith(testDomain.name);
+      expect(mockDbService.domain.update).toHaveBeenCalledWith({
+        where: { id: testDomain.id },
+        data: { expirationDate: testExpirationDateDomain.expirationDate },
+      });
+    });
+
+    it('should throw NotFoundException when domain is not found or user id does not match', async () => {
+      mockDbService.domain.findUnique.mockResolvedValueOnce(null);
+      await expect(service.updateExpirationDate(testDomain.id, 'wrong-user-id')).rejects.toThrow(NotFoundException);
+      expect(mockDbService.domain.findUnique).toHaveBeenCalledWith({ where: { id: testDomain.id } });
+    });
+
+    it('should throw HttpException when could not get the expiration date', async () => {
+      mockExpirationService.getExpirationDate.mockResolvedValueOnce(null);
+      await expect(service.updateExpirationDate(testDomain.id, testDomain.userId)).rejects.toThrow(HttpException);
+      expect(mockDbService.domain.findUnique).toHaveBeenCalledWith({ where: { id: testDomain.id } });
+      expect(mockExpirationService.getExpirationDate).toHaveBeenCalledWith(testDomain.name);
+    });
+  });
 });
+
+const testDomain = {
+  id: '1',
+  name: 'example.com',
+  userId: 'test-user-id',
+};
+
+const mockDbService = {
+  domain: {
+    create: jest.fn().mockResolvedValue(testDomain),
+    findMany: jest.fn().mockResolvedValue([testDomain]),
+    findFirst: jest.fn().mockResolvedValue(testDomain),
+    findUnique: jest.fn().mockResolvedValue(testDomain),
+    delete: jest.fn().mockResolvedValue(testDomain),
+    update: jest.fn().mockResolvedValue(testDomain),
+  },
+};
+
+const mockExpirationService = {
+  getExpirationDate: jest.fn().mockResolvedValue(new Date()),
+};
+
+const testExpirationDateDomain = {
+  ...testDomain,
+  expirationDate: new Date(),
+};
--- src/domains/domains.service.ts
+++ src/domains/domains.service.ts
@@ -1,10 +1,11 @@
-import { Global, Injectable, NotFoundException } from '@nestjs/common';
+import { Global, HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { DbService } from '@src/db/db.service';
+import { DomainsExpirationService } from './domains-expiration/domains-expiration.service';
 
 @Global()
 @Injectable()
 export class DomainsService {
-  constructor(private readonly db: DbService) {}
+  constructor(private readonly db: DbService, private readonly expirationService: DomainsExpirationService) {}
 
   public async create(domain: string, userId: string) {
     return this.db.domain.create({
@@ -54,6 +55,32 @@ export class DomainsService {
     return this.db.domain.delete({
       where: {
         id,
+      },
+    });
+  }
+
+  public async updateExpirationDate(id: string, userId: string) {
+    const domain = await this.db.domain.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!domain || domain.userId !== userId) {
+      throw new NotFoundException(`Domain with id ${id} not found`);
+    }
+
+    const expirationDate = await this.expirationService.getExpirationDate(domain.name);
+    if (!expirationDate) {
+      throw new HttpException('Could not get expiration date', 424);
+    }
+
+    return this.db.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        expirationDate,
       },
     });
   }
