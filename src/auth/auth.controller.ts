65e0d0c43fa2ba710e4e95bf1bee0bea26114f54
Jakub Surdej <jakub.surdej@codahead.com>
2023-05-31 13:08:57 +0200

    feat: implement proper user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './guards/local.guard';
+import { Body, Controller, Logger, Post, UnauthorizedException } from '@nestjs/common';
+import { UserLoginDto } from './dto/userLoginDto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(LocalAuthGuard)
+  private readonly logger = new Logger(AuthController.name);
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
-  async login() {
-    return 'success';
+  async login(@Body() loginDto: UserLoginDto) {
+    this.logger.debug(`Login attempt for ${loginDto.email}`);
+    const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+    if (!user) {
+      this.logger.debug(`Login attempt failed for ${loginDto.email}`);
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    this.logger.log(`Login attempt succeeded for ${loginDto.email}`);
+    return this.authService.login(user);
   }
 }
---
+++ src/auth/dto/userLoginDto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class UserLoginDto {
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'test@example.com',
+  })
+  readonly email!: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Password of the user',
+    example: 'password',
+  })
+  readonly password!: string;
+}
