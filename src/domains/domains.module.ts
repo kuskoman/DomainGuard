22ecf78b505b7138a4e00990f18f15ab6e32f547
Jakub <kubasurdej@gmail.com>
2023-06-06 21:09:16 +0200

    fix: add missing encryption module

--- src/domains/domains.module.ts
+++ src/domains/domains.module.ts
@@ -3,10 +3,11 @@ import { DomainsService } from './domains.service';
 import { DomainsExpirationModule } from './domains-expiration/domains-expiration.module';
 import { SslCertificatesModule } from './ssl-certificates/ssl-certificates.module';
 import { DomainsController } from './domains.controller';
+import { EncryptionModule } from '@src/encryption/encryption.module';
 
 @Module({
   providers: [DomainsService],
-  imports: [DomainsExpirationModule, SslCertificatesModule],
+  imports: [DomainsExpirationModule, SslCertificatesModule, EncryptionModule],
   controllers: [DomainsController],
 })
 export class DomainsModule {}
