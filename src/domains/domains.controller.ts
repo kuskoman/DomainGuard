69b5f4c9d92018230bf0f71df0fde5b29d599575
Jakub Surdej <jakub.surdej@codahead.com>
2023-06-08 15:37:41 +0200

    refactor: use UserId decorator in domains controller

--- src/domains/domains.controller.ts
+++ src/domains/domains.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post, Get, Req, UseGuards, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Get, UseGuards, NotFoundException } from '@nestjs/common';
 import { CreateDomainDto } from './dto/createDomainDto';
 import { DomainsService } from './domains.service';
 import { LoggedGuard } from '@src/auth/guards/logged.guard';
@@ -10,7 +10,6 @@ export class DomainsController {
 
   constructor(private readonly domainsService: DomainsService) {}
 
-  // todo: create a decorator for getting user from request
   @UseGuards(LoggedGuard)
   @Post()
   create(@Body() { name }: CreateDomainDto, @UserId() userId: string) {
@@ -20,17 +19,17 @@ export class DomainsController {
 
   @UseGuards(LoggedGuard)
   @Get()
-  findAll(@Req() req: Request & { userId: string }) {
-    this.logger.log(`Getting all domains for user ${req.userId}`);
-    return this.domainsService.findAllWithUser(req.userId);
+  findAll(@UserId() userId: string) {
+    this.logger.log(`Getting all domains for user ${userId}`);
+    return this.domainsService.findAllWithUser(userId);
   }
 
   @UseGuards(LoggedGuard)
   @Get(':id')
-  async findOne(@Req() req: Request & { userId: string }, @Body() { id }: { id: string }) {
-    this.logger.log(`Getting domain ${id} for user ${req.userId}`);
-    const domain = await this.domainsService.findOneWithUser(id, req.userId);
-    if (!domain || domain.userId !== req.userId) {
+  async findOne(@UserId() userId: string, @Body() { id }: { id: string }) {
+    this.logger.log(`Getting domain ${id} for user ${userId}`);
+    const domain = await this.domainsService.findOneWithUser(id, userId);
+    if (!domain || domain.userId !== userId) {
       throw new NotFoundException(`Domain ${id} not found`);
     }
 
@@ -39,13 +38,13 @@ export class DomainsController {
 
   @UseGuards(LoggedGuard)
   @Post(':id/refresh')
-  async refreshDomainExpiration(@Req() req: Request & { userId: string }, @Body() { id }: { id: string }) {
-    this.logger.log(`Refreshing domain ${id} for user ${req.userId}`);
-    const domain = await this.domainsService.findOneWithUser(id, req.userId);
-    if (!domain || domain.userId !== req.userId) {
+  async refreshDomainExpiration(@UserId() userId: string, @Body() { id }: { id: string }) {
+    this.logger.log(`Refreshing domain ${id} for user ${userId}`);
+    const domain = await this.domainsService.findOneWithUser(id, userId);
+    if (!domain || domain.userId !== userId) {
       throw new NotFoundException(`Domain ${id} not found`);
     }
 
-    return this.domainsService.updateDomainExpirationDateWithUser(id, req.userId);
+    return this.domainsService.updateDomainExpirationDateWithUser(id, userId);
   }
 }
