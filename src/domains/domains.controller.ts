29103067144c937c5d6dfc9980a23c922cc73fa9
Jakub Surdej <jakub.surdej@codahead.com>
2023-06-08 15:43:03 +0200

    refactor: add ApiHeader to domains controller

--- src/domains/domains.controller.ts
+++ src/domains/domains.controller.ts
@@ -3,28 +3,28 @@ import { CreateDomainDto } from './dto/createDomainDto';
 import { DomainsService } from './domains.service';
 import { LoggedGuard } from '@src/auth/guards/logged.guard';
 import { UserId } from '@src/auth/decorators/userId.decorator';
+import { ApiHeader } from '@nestjs/swagger';
 
+@UseGuards(LoggedGuard)
+@ApiHeader({ name: 'Authorization', description: 'JWT received from login endpoint', example: 'Bearer <Token>' })
 @Controller('domains')
 export class DomainsController {
   private readonly logger = new Logger(DomainsController.name);
 
   constructor(private readonly domainsService: DomainsService) {}
 
-  @UseGuards(LoggedGuard)
   @Post()
   create(@Body() { name }: CreateDomainDto, @UserId() userId: string) {
     this.logger.log(`Creating domain ${name}`);
     return this.domainsService.create(name, userId);
   }
 
-  @UseGuards(LoggedGuard)
   @Get()
   findAll(@UserId() userId: string) {
     this.logger.log(`Getting all domains for user ${userId}`);
     return this.domainsService.findAllWithUser(userId);
   }
 
-  @UseGuards(LoggedGuard)
   @Get(':id')
   async findOne(@UserId() userId: string, @Body() { id }: { id: string }) {
     this.logger.log(`Getting domain ${id} for user ${userId}`);
@@ -36,7 +36,6 @@ export class DomainsController {
     return domain;
   }
 
-  @UseGuards(LoggedGuard)
   @Post(':id/refresh')
   async refreshDomainExpiration(@UserId() userId: string, @Body() { id }: { id: string }) {
     this.logger.log(`Refreshing domain ${id} for user ${userId}`);
