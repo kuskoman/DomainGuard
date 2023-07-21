f4d8d7473a77769349c994e8977f1d42630dc50a
Jakub Surdej <jakub.surdej@codahead.com>
2023-07-21 12:58:55 +0200

    chore(backend): enable cors to all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,20 @@ import { AppModule } from './app.module';
 import { BaseConfig, baseConfig } from './config/base.config';
 import 'source-map-support/register';
 import { setupSwagger } from './swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const bootstrap = async () => {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  // Add this section to enable CORS
+  const corsOptions: CorsOptions = {
+    origin: '*', // Change this to specific origin(s) if required
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+  app.enableCors(corsOptions);
+
   app.useGlobalPipes(new ValidationPipe());
   const { port, swaggerEnabled } = app.get<BaseConfig>(baseConfig.KEY);
 
