966ff36352a98d617c6761bca534cbe7afa09e3e
Jakub Surdej <jakub.surdej@codahead.com>
2023-07-26 14:00:17 +0200

    fix(frontend): add authorization header to the request

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from '@/stores/user'
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3000/'
@@ -9,3 +10,19 @@ export const apiClient = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const userStore = useUserStore()
+
+    if (userStore?.accessToken) {
+      config.headers.Authorization = `Bearer ${userStore.accessToken}`
+    }
+
+    return config
+  },
+
+  (error) => {
+    return Promise.reject(error)
+  }
+)
