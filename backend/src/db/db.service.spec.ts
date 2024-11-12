bf7114560c4f0c0064925d8bf7908ca57514c263
Jakub Surdej <kubasurdej@gmail.com>
2024-11-12 23:36:02 +0100

    build: upgrade multiple dependencies

--- backend/src/db/db.service.spec.ts
+++ backend/src/db/db.service.spec.ts
@@ -1,17 +1,14 @@
 import { Test } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import { DbService } from './db.service';
 
 describe(DbService.name, () => {
   let service: DbService;
-  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       providers: [DbService],
     }).compile();
 
-    app = moduleRef.createNestApplication();
     service = moduleRef.get<DbService>(DbService);
   });
 
@@ -22,20 +19,6 @@ describe(DbService.name, () => {
       await service.onModuleInit();
 
       expect(connectSpy).toHaveBeenCalled();
-    });
-  });
-
-  describe('enableShutdownHooks', () => {
-    it('should close the app before exiting', async () => {
-      const beforeExitSpy = jest.spyOn(service, '$on');
-      const closeSpy = jest.spyOn(app, 'close').mockResolvedValue();
-
-      await service.enableShutdownHooks(app);
-
-      expect(beforeExitSpy).toHaveBeenCalled();
-      await app.close();
-
-      expect(closeSpy).toHaveBeenCalled();
     });
   });
 });
--- backend/src/domains/ssl-certificates/ssl-certificates.spec.ts
+++ backend/src/domains/ssl-certificates/ssl-certificates.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SslCertificatesService } from './ssl-certificates.service';
 
-describe('SslCertificatesService', () => {
+describe(SslCertificatesService.name, () => {
   let service: SslCertificatesService;
 
   beforeEach(async () => {
@@ -18,17 +18,36 @@ describe('SslCertificatesService', () => {
 
   describe('checkSslExpiration', () => {
     describe('online test', () => {
-      it.todo('should return the correct certificate expiration date when certificate is valid');
+      it('should return the correct certificate expiration date when the certificate is valid', async () => {
+        const domain = 'www.google.com';
+        const expirationDate = await service.checkSslExpiration(domain);
+        expect(expirationDate).toBeInstanceOf(Date);
+        expect(expirationDate!.getTime()).toBeGreaterThan(Date.now());
+      });
     });
 
     describe('offline test', () => {
-      it.todo('should return the correct certificate expiration date when certificate is valid');
+      it('should return the correct certificate expiration date when certificate is valid', async () => {
+        jest.spyOn(service, 'checkSslExpiration').mockResolvedValue(new Date('2024-12-31T23:59:59Z'));
+        const expirationDate = await service.checkSslExpiration('mockdomain.com');
+        expect(expirationDate).toEqual(new Date('2024-12-31T23:59:59Z'));
+      });
 
       it.todo('should throw an error when the certificate is invalid or expiration date cannot be retrieved');
 
-      it.todo('should throw an error if the request encounters an error');
+      it('should throw an error if the request encounters an error', async () => {
+        jest.spyOn(service, 'checkSslExpiration').mockRejectedValue(new Error('Request error'));
 
-      it.todo('should throw an error if the socket encounters an error');
+        await expect(service.checkSslExpiration('error.com')).rejects.toThrow('Request error');
+      });
+
+      it('should throw an error if the socket encounters an error', async () => {
+        jest.spyOn(service, 'checkSslExpiration').mockImplementation(() => {
+          return new Promise((_, reject) => reject(new Error('Socket error')));
+        });
+
+        await expect(service.checkSslExpiration('socketerror.com')).rejects.toThrow('Socket error');
+      });
     });
   });
 });
