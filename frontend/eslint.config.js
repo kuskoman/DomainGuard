11c7e77b535659d0022a5324f9c6d99781d1eeb9
Jakub Surdej <jakub.sudej@shift4.com>
2024-11-22 21:01:32 +0100

    chore(frontend): configure prettier to work with eslint, change port

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,36 +1,39 @@
-/**
- * .eslint.js
- *
- * ESLint configuration file.
- */
-
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
 
-  ...pluginVue.configs['flat/recommended'],
+  ...pluginVue.configs["flat/recommended"],
   ...vueTsEslintConfig(),
 
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
-      '@typescript-eslint/no-unused-expressions': [
-        'error',
+      ...prettierConfig.rules, // Disable ESLint rules that conflict with Prettier
+      "prettier/prettier": "error", // Run Prettier as an ESLint rule
+
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
         {
           allowShortCircuit: true,
           allowTernary: true,
         },
       ],
-      'vue/multi-word-component-names': 'off',
-    }
-  }
-]
+      "vue/multi-word-component-names": "off",
+      "vue/max-attributes-per-line": "off",
+    },
+  },
+];
